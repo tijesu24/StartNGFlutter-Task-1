e142df0717af1e04bf1e5703710e97cd19d297ac
tijesu24 <tijesu24@gmail.com>
2020-03-26 11:38:18 +0100

    This is the flutter project

---
+++ android/app/src/main/kotlin/com/example/sng_proj1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sng_proj1
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
